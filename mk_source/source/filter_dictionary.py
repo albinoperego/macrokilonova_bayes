dcbe3b4c746b7d6600f17e6bde6244a52707e6cf
Albino <alvin.0983@gmail.com>
2017-11-25 23:26:22 +0100

    Addition of filters and observed magnitude files

---
+++ mk_source/source/filter_dictionary.py
@@ -0,0 +1,136 @@
+import numpy as np
+
+filters = {}
+
+filters[2190] ={ 'name'  : 'K',
+                 'lambda': 2190e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_K.dat']
+             }
+
+filters[445]  ={ 'name'  : 'B',
+                 'lambda': 445e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_B.dat']
+             }
+
+filters[365] ={ 'name'  : 'U',
+                 'lambda': 365e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_U.dat']
+             }
+
+filters[337] ={ 'name'  : 'U_HST',
+                 'lambda': 337.5e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_F336W.dat']
+             }
+
+filters[1005] ={ 'name'  : 'y',
+                 'lambda': 1005e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_y.dat']
+             }
+
+filters[1020] ={ 'name'  : 'Y',
+                 'lambda': 1020e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_Y.dat']
+             }
+
+filters[354]  ={ 'name'  : 'u',
+                 'lambda': 354e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_u.dat']
+             }
+
+filters[475]  ={ 'name'  : 'g',
+                 'lambda': 475e-9,
+                 'active': 1,
+                 'filename': ['filter_measures_g.dat','filter_measures_F475W.dat']
+             }
+
+filters[850]  ={ 'name'  : 'z',
+                 'lambda': 850e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_z.dat','filter_measures_F850W.dat']
+             }
+
+filters[551]  ={ 'name'  : 'V',
+                 'lambda': 551e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_V.dat']
+             }
+
+filters[596]  ={ 'name'  : 'V_HST',
+                 'lambda': 595.6e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_F606W.dat']
+             }
+
+
+filters[1220] ={ 'name'  : 'J',
+                 'lambda': 1220e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_J.dat']
+             }
+
+filters[10469] ={ 'name'  : 'J',
+                 'lambda': 1046.9e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_J1.dat']
+             }
+
+filters[1150] ={ 'name'  : 'J_HST',
+                 'lambda': 1150e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_F110W.dat']
+             }
+
+filters[2150] ={ 'name'  : 'Ks',
+                 'lambda': 2150e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_Ks.dat']
+             }
+
+filters[1630] ={ 'name'  : 'H',
+                 'lambda': 1630e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_H.dat']
+             }
+
+filters[1545] ={ 'name'  : 'H_HST',
+                 'lambda': 1545e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_F160W.dat']
+             }
+
+filters[658]  ={ 'name'  : 'R',
+                 'lambda': 658e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_R.dat']
+             }
+
+filters[625]  ={ 'name'  : 'r',
+                 'lambda': 625e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_r.dat','filter_measures_F625W.dat']
+             }
+
+filters[12130]={ 'name'  : 'C',
+                 'lambda': 12130e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_C.dat']
+             }
+
+filters[806]  ={ 'name'  : 'I',
+                 'lambda': 806e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_I.dat','filter_measures_F814W.dat']
+               }
+
+filters[775]  ={ 'name'  : 'i',
+                 'lambda': 775e-9,
+                 'active': 0,
+                 'filename': ['filter_measures_i.dat','filter_measures_F775W.dat']
+               }
--- mk_source/source/filters.py
+++ mk_source/source/filters.py
@@ -1,14 +1,16 @@
 import numpy as np
+import filter_dictionary
+import copy
 
 class Filter(object):
 
-    def __init__(self, name, nu_central, width, filename=None):
+#    def __init__(self, name, nu_central, width, filename=None):
 
-        self.name = name
-        self.nu_central = nu_central
-        self.width = width
-        if filename is not None:
-            self.t, self.magnitude, self.magnitude_error = self.read_measurements(filename)
+#        self.name = name
+#        self.nu_central = nu_central
+##        self.width = width
+#        if filename is not None:
+#            self.t, self.magnitude, self.magnitude_error = self.read_measurements(filename)
     
     def read_measurements(self, filename):
         self.t, self.magnitude, self.magnitude_error = np.loadtxt(filename, unpack = True)
@@ -19,9 +21,35 @@ class Filter(object):
         return self.res
 
 if __name__=="__main__":
-    names = ['K','M']
-    freqs = [500,700]
-    widths = [10,20]
 
-    misure = [Filter(n,l,w) for n,l,w in zip(names,freqs,widths)]
-    logL = -0.5*np.sum([m.residuals(modello)**2 for m in misure])
+    dic_filt = filter_dictionary.filters
+    lambda_vec = copy.deepcopy(dic_filt.keys())
+    lambda_vec.sort()
+
+    f = Filter()
+
+
+    measures={}
+    for ilambda in lambda_vec:
+        if (dic_filt[ilambda]["active"]==1):
+           measures[ilambda] = {}
+           t_tot = np.asarray([])
+           m_tot = np.asarray([])
+           sm_tot = np.asarray([])
+           for fname in dic_filt[ilambda]["filename"]:
+               print(fname)
+               t,m,sm = f.read_measurements("filter_data/"+fname)
+               print(t)
+               t_tot = np.append(t_tot,t)
+               m_tot = np.append(m_tot,m)
+               sm_tot = np.append(sm_tot,sm)
+
+           measures[ilambda]['time']  = t_tot
+           measures[ilambda]['mag']   = m_tot
+           measures[ilambda]['sigma'] = sm_tot
+           measures[ilambda]['name']  = dic_filt[ilambda]["name"]
+               
+    print(measures)           
+
+#    misure = [Filter(n,l,w) for n,l,w in zip(names,freqs,widths)]
+#    logL = -0.5*np.sum([m.residuals(modello)**2 for m in misure])
